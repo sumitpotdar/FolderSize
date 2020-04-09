00420ff182dff63822f57bbfc2ec966ea4efddc8
sumitpotdar <sumitseems@gmail.com>
2020-04-09 15:08:22 -0500

    Add files via upload

---
+++ FolderSize.ps1
@@ -0,0 +1,13 @@
+﻿Install-Module -Name PSFolderSize -RequiredVersion 1.0 
+
+function Folder_Size {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$RootPath
+    )
+    Get-FolderSize -BasePath $RootPath | Export-Csv C:\Temp\foldersize.csv -NoTypeInformation
+    Write-Host "I am done Yippee!"
+}
+
+Folder_Size
